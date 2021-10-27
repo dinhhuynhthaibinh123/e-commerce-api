abf958478f0cf2f2f9545bd7e532d22c78967d04
ChovyDoinb <binhdinhqt137@gmail.com>
2021-10-28 02:00:47 +0700

    API

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db/index");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const productRoute = require("./routes/product");
+const cors = require("cors");
+
+dotenv.config();
+connectDB.connect();
+
+//Middeware
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
